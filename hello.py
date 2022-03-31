c3e4e7134632918bd6c3e2daef099db788c3f5f5
minseo22029165 <102271676+minseo22029165@users.noreply.github.com>
2022-03-31 11:34:23 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 // For command line git tutorial
 // First: GitHub modification
 // Second: Local repository modification
+  
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
-print("Tell her world")
+print("Tell my world")
